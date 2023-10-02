864ea39b8f7580ea29584280322e5f605b6b9182
WarLoRDlArS <sequeiralarsondev@gmail.com>
2023-10-02 13:48:45 +0530

    Hadnt added certain files in previous commit

--- Client/main.py
+++ Client/main.py
@@ -1,13 +1,69 @@
-
+import socket
+import threading
+import sys
+import time
 
 switchCase = {
     1 : "Sign In",
     2: "Log In"
 }
 
+def ClientConnect(serverIP,ServerPort):
+    """This function is used to Connect Client to the server
+    """
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as clientConn:
+        try:
+            clientConn.connect((serverIP,ServerPort))  
+            print(clientConn)
+            return clientConn
+        except:
+            print("Couldnt Connect to server") 
+            sys.exit()
+
+def sendMessage(ClientConn,message): 
+    while True: 
+        print("Enter message: ")
+        message=input()
+        if message.lower() == 'exit':
+            break
+        try: 
+            ClientConn.send(message.encode('utf-8'))
+        except:
+            print("Couldnt Send Message: " + message)  
+        time.sleep(1)
+
+def recMessage(clientSock):
+    while True:
+        try:
+            messageBuffer = b''
+            while True:
+                chunk = clientSock.recv(1024)
+                if not chunk:
+                    break
+                messageBuffer += chunk
+
+            if messageBuffer:
+                message = messageBuffer.decode('utf-8')
+                print(message)
+                #return message
+
+        except Exception as e:
+            print(f"Error in receiving message: {str(e)}")
+            break
 
 if __name__=="__main__":
-    print("What WOuld You Like To Do: ")
+    print("Enter Server IP:")
+    serverIP=input()
+    print("Enter Server Port:")
+    serverPort=int(input())
+    clientconn=ClientConnect(serverIP,serverPort)
+    try:
+        send = threading.Thread(target=sendMessage, args=(clientconn, "hello"))
+        recv = threading.Thread(target=recMessage, args=(clientconn,))
+        send.start()
+        recv.start()
+    except:
+        print("Couldnt create threads")
+    print("What Would You Like To Do: ")
     for x,y in switchCase.items():
         print(x, y)
-    
--- Server/Server.py
+++ Server/Server.py
@@ -1,8 +1,9 @@
 import socket
 import threading
+from Main import *
 
 def portSearch(ServerSock,ServerIP):
-    port=1025
+    port=1026
     while port<66000:
         try:
             ServerSock.bind((ServerIP,port))
@@ -11,16 +12,56 @@ def portSearch(ServerSock,ServerIP):
             port+=1
     return port
 
-def SendMessage():
-    pass
+def SendMessage(ClientSock,message):
+    while True: 
+        try: 
+            ClientSock.send(message.encode('utf-8'))
+        except Exception as e:
+            print(f"Exception in sending message:{message}") 
+            print(e)
 
-def RecvMessage():
-    pass
+def RecvMessage(ClientSock):
+        while True:
+            try:
+                messageBuffer=b''
+                while True:
+                    chunk=ClientSock.recv(1024)
+                    if not chunk:
+                        break
+                    messageBuffer+=chunk
+                if not messageBuffer:
+                    break
+                message=messageBuffer.decode('utf-8')
+                print(message)
+                return message
+                
+            except Exception as e:
+                print("Exception in recieveing message\n\n")
+                print(f"Error: {str(e)}") 
+    
+
 
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as serverSocket:
     ServerIP = socket.gethostbyname(socket.gethostname()) 
     #bind to the port
     #this function will also bind the socket
     ServerPort= portSearch(serverSocket,ServerIP)
+    try:
+        serverSocket.bind((ServerIP,ServerPort))
+    except:
+        print("Server already binded")
+
     print(f"IP:{ServerIP}\tPort:{ServerPort}")
 
+    serverSocket.listen()
+    clientSocket,Clientadd=serverSocket.accept()
+    print(f"Client {Clientadd} connected")
+    
+    try:
+        recieve=threading.Thread(target=RecvMessage,args=(clientSocket,))
+        send=threading.Thread(target=SendMessage,args=(clientSocket,"hello from server"))
+        recieve.start()
+        send.start()
+    except:
+        print("Couldnt create threads")
+    print("Hello")
